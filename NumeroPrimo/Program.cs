e40b008ae6ae1c21c9bf37b367a1bd6a97970e8f
jhonatacamargol <jhonatapires32@hotmail.com>
2022-12-06 19:37:21 -0300

    Add project files.

---
+++ NumeroPrimo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeroPrimo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 3, divivible = 0;
+            Console.WriteLine("Digite um número máximo para imprimir os números primos!");
+            int numero_final = int.Parse(Console.ReadLine());
+            while (numero <= numero_final) 
+            {
+                for (int i = 1; i <= numero; i++)
+                {
+                    if (numero % i == 0)
+                    {
+                        divivible++;
+                    }
+                    if (divivible > 2)
+                    {
+                        break;
+                    }
+                }
+                 if (divivible==2)
+                 {
+                            Console.Write("{0},",numero);
+                 }
+                 divivible = 0;
+                numero++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
